f8742e576bfdbb3af2f6485ed464b2a8526c8350
MarcosDAntony <marcosantony.asp@gmail.com>
2025-08-11 11:57:54 -0300

    11-08-25

--- Engenharia/Estudos/Computação/Curso-em-Video/Python/Mundo-I/exercicios/04.py
+++ Engenharia/Estudos/Computação/Curso-em-Video/Python/Mundo-I/exercicios/04.py
@@ -1,5 +1,14 @@
 # Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
-algo = input(int(bool(float(str("Digite algo:")))))
+algo = input("Digite algo: ")
 
-print("Você digitou: ",algo, " e é do tipo ", type(algo))
+print("O tipo primitivo do valor dado é ", type(algo))
+print("É numérico?", algo.isnumeric())
+print("É alfabético?", algo.isalpha())
+print("É alfanumérico?", algo.isalnum())
+print("Tem só letras maiusculas?", algo.isupper())
+print("Tem só letras minisuculas?", algo.islower())
+print("Está titularizado?", algo.istitle())
+print("É um número decimal?", algo.isdecimal())
+print("Só tem espaços?", algo.isspace())
+print("Está dentro do padrão ASCII de 7 bits?", algo.isascii())
